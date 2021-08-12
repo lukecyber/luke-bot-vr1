0c9765fc652ebd2c86bf3fc92ade868ed9b3b864
luke <84767414+lukecyber@users.noreply.github.com>
2021-08-12 17:06:30 +0530

    Update conventer.js

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -55,7 +55,7 @@ function webp2mp4File(path) {
                 const result = 'https:' + $('div#output > p.outfile > video > source').attr('src')
                 resolve({
                     status: true,
-                    message: "Made by Sunaif Adkar 🏌️",
+                    message: "Made by luke",
                     result: result
                 })
             }).catch(reject)
