a766a592b5a35fcc03edcf720b7598584377bf11
luke <84767414+lukecyber@users.noreply.github.com>
2021-09-16 11:57:04 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -97,7 +97,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by luke 🏌️Public'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by luke #contact me at https://wa.me/+15063009861'})
 
     }));
 }
@@ -191,7 +191,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by luke 🏌️Public'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by luke #contact me at https://wa.me/+15063009861'})
 
     }));
 }
